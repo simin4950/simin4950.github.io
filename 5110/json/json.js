330ed23912b657fa81158d7c850fa323e3fda60a
Simin <simin4950@gmail.com>
2020-11-03 23:50:36 -0500

    About my class week 10 assignment

---
+++ 5110/json/json.js
@@ -0,0 +1,15 @@
+let students = document.querySelector("#students");
+// var theAnswer = "";
+
+
+$(document).ready(function() {
+    $.getJSON('https://raw.githubusercontent.com/emuel/api/master/5110', function(data) {
+            for(var i = 0; i < data.length; i++){
+                students.innerHTML += "<div id='student'><div id='name'>" + data[i].stu_name + " " + data[i].last_name + "</div>" 
+                    + "<br><img src=http://" + data[i].domain + "/images/me.jpg><br>" 
+                    + "<div id='fact'>" + data[i].fact + "</div><br></div>";
+                // category.innerHTML = "Category: " + data[0].category.title;
+                // theAnswer = data[0].answer;
+            } 
+        });
+});
