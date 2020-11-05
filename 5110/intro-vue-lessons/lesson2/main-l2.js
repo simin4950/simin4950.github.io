4c997cdcafa2e631c9cbe614a8c0e9fb8575c082
Simin <simin4950@gmail.com>
2020-11-04 22:26:04 -0500

    Completed Vue Lessons 1-4

---
+++ 5110/intro-vue-lessons/lesson2/main-l2.js
@@ -0,0 +1,8 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'These socks are made from wool that will keep you warm in the cold, harsh winters.'
+        }
+    }
+})
