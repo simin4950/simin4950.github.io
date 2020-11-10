405b221bc036080af2bb136acfdfa886d899d3e4
Simin <simin4950@gmail.com>
2020-11-10 15:01:46 -0500

    Vue lessons

--- 5110/intro-vue-lessons/lesson4/main.js
+++ 5110/intro-vue-lessons/lesson4/main.js
@@ -4,7 +4,7 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inventory: 10,
-            onSale: false
+            onSale: true
         }
     }
 })
