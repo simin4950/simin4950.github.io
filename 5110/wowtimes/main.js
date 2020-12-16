65b4d51fb20a0e054a6cc90c8aaee72d7ed5370e
Simin Savani <simin4950@gmail.com>
2020-12-16 02:09:38 -0500

    Create WOW!Times E-Com Business Template

---
+++ 5110/wowtimes/main.js
@@ -0,0 +1,12 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            cart:0,
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+    },
+})
